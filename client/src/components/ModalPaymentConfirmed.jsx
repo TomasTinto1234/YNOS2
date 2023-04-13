32107fb769d12c17d522571cf31d950a09e8b271
TomasTinto1234 <tititinto@gmail.com>
2023-04-13 12:13:32 -0300

    pay modal

---
+++ client/src/components/ModalPaymentConfirmed.jsx
@@ -0,0 +1,131 @@
+import { StyleSheet, View, Text, Modal, Pressable } from 'react-native'
+import { fonts, colors } from '../utils/theme'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+// import Confeti from '../../assets/confeti'
+import { useNavigate } from 'react-router-native'
+
+const ModalPaymentConfirmed = ({ paymentCode, modalVisible, setModalVisible, numeros }) => {
+
+  const navigation = useNavigate()
+  const handleRediret = () => {
+    setModalVisible(false)
+    navigation('/')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.')
+          setModalVisible(!modalVisible)
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {/* <Confeti style={styles.iconModal} /> */}
+
+            <Text style={styles.modalText}>Pago Confirmado</Text>
+            <Text style={styles.modalText2}>Código de pago: #{numeros}</Text>
+            <View style={styles.buttonModalContainer}>
+              <Pressable style={styles.button} onPress={handleRediret}>
+                <Text style={styles.textStyle}>Revisar Comprobante</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  // modal container
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  // modal
+  modalView: {
+    width: wp('80.00%'),
+    height: hp('50.00%'),
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  // modal button
+  button: {
+    borderRadius: 5,
+    padding: 10,
+    elevation: 5,
+    width: wp('60.00%'),
+    height: hp('6.00%'),
+    backgroundColor: colors.primaryTomato,
+  },
+
+  buttonModalContainer: {
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: hp('5%'),
+  },
+
+  modalButton: {
+    width: wp('20.00%'),
+    height: hp('5.00%'),
+  },
+  // modal button text
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: wp('4.00%'),
+    fontFamily: fonts.montserrat.regular,
+    fontWeight: '500',
+  },
+  modalText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    fontFamily: fonts.montserrat.medium,
+    fontWeight: '400',
+    marginBottom: 15,
+    textAlign: 'center',
+    marginTop: hp('3%'),
+    color: colors.mediumseagreen,
+  },
+  modalText2: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    fontFamily: fonts.montserrat.medium,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: hp('-2%'),
+    color: colors.secundary8,
+  },
+
+  iconModal: {
+    width: wp('50.00%'),
+    height: hp('20.00%'),
+  },
+})
+
+export default ModalPaymentConfirmed
--- client/src/screens/PayScreen.jsx
+++ client/src/screens/PayScreen.jsx
@@ -1,4 +1,5 @@
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View, Pressable,Text } from 'react-native'
+import React, { useState } from 'react'
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
@@ -9,8 +10,26 @@ import BigButtonPay from '../components/BigButtonPay'
 // import TotalCost from '../components/TotalCost'
 // import OrdersFromOthersMembers from '../components/OrdersFromOthersMembers'
 // import TypesOfPayments from '../components/TypesOfPayments'
+import ModalPaymentConfirmed from "../components/ModalPaymentConfirmed"
+import { fonts, colors } from '../utils/theme'
 
 const PayScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const handlePay = () => {
+    setModalVisible(true)
+  }
+  const [numeros, setNumeros] = React.useState([]);
+
+  const generarNumerosAleatorios = () => {
+    const nuevosNumeros = [];
+    for (let i = 0; i < 5; i++) {
+      // Genera un número aleatorio entre 1 y 100
+      const nuevoNumero = Math.floor(Math.random() * 10) + 1;
+      nuevosNumeros.push(nuevoNumero);
+    }
+    setNumeros(nuevosNumeros);
+  };
   return (
     <View style={styles.container}>
       <TitlePage text='Pagar' route='/vieworder' white={false} />
@@ -22,9 +41,16 @@ const PayScreen = () => {
           {/* <TotalCost sliceState={'totalPrice'} /> */}
         </ScrollView>
       </View>
-      <View style={styles.btn}>
+      <ModalPaymentConfirmed modalVisible={modalVisible} setModalVisible={setModalVisible} numeros= {numeros} />
+      <Pressable
+        style={styles.pressButton}
+        onPress={() => handlePay(generarNumerosAleatorios())}
+      >
+        <Text style={styles.text}>Pagar</Text>
+      </Pressable>
+      {/* <View style={styles.btn}>
         <BigButtonPay text='Pagar' route='/' />
-      </View>
+      </View> */}
     </View>
   )
 }
@@ -52,5 +78,50 @@ const styles = StyleSheet.create({
   visibleBox: {
     height: hp(79),
     alignItems: 'center'
-  }
+  },
+
+
+  buttonModalContainer: {
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: hp('5%'),
+  },
+
+  modalButton: {
+    width: wp('20.00%'),
+    height: hp('5.00%'),
+  },
+  // modal button text
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: wp('4.00%'),
+    fontFamily: fonts.montserrat.regular,
+    fontWeight: '500',
+  },
+  modalText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    fontFamily: fonts.montserrat.medium,
+    fontWeight: '400',
+    marginBottom: 15,
+    textAlign: 'center',
+    marginTop: hp('3%'),
+    color: colors.mediumseagreen,
+  },
+  modalText2: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    fontFamily: fonts.montserrat.medium,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: hp('-2%'),
+    color: colors.secundary8,
+  },
+
+  iconModal: {
+    width: wp('50.00%'),
+    height: hp('20.00%'),
+  },
 })
